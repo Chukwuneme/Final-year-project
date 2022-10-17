2701613359e10b4f4c3c5c17c015b67b5dc2df8e
chukwuneme <chukwunemetadinma@gmail.com>
2022-10-17 00:37:31 -0700

    Updated the code to work better for interview

--- flaskforhtml.py
+++ flaskforhtml.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
-import cv2
+#import cv2
 import os
 import numpy as np
 from sklearn.linear_model import LogisticRegression
@@ -8,7 +8,7 @@ import joblib
 
 
 logisticRegr = joblib.load('c:/users/johnpaul/desktop/finalized_model.sav')
-#predictions2 = logisticRegr.predict(test_files)
+
 
 app = Flask(__name__,  static_folder='static')
 
@@ -17,30 +17,40 @@ app = Flask(__name__,  static_folder='static')
 def skincancer():
 	if request.method == 'POST':
 		uploaded_file = request.files['image']
-		uploaded_file.filename != ''
-		#os.remove("default.png")
+		
 		num = 1
 		num = num + 1
 		name = "default" + str(num)
 		filenn = "static/" + name + ".png"
+
 		uploaded_file.save(filenn)
 		image = cv2.imread(filenn)
+
 		image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 		ggg = "greay"
 		hhh = "static/" + ggg + ".png"
 		cv2.imwrite(hhh, image)
+
 		image = cv2.imread(hhh)
 		image = cv2.resize(image, (440, 440))
 		image_flattened = image.flatten()
 		flattened = []
 		flattened.append(image_flattened)
+
 		flattened = np.array(flattened)
 		predictions2 = logisticRegr.predict(flattened)
 		if predictions2[0] == 0:
 			value = "benign"
+			proba = logisticRegr.predict_proba[0][0]
+			advice = "You need to see thee doctor asap"
 		else:
 			value = "malignant"
-	return render_template('secondpage.html', test=filenn, predictions = value)
+			proba = logisticRegr.predict_proba[0][1]
+			advice = "Your cancer might be malignant. you need urgent medical attention"
+
+		
+
+	return render_template('secondpage.html', test=filenn, predictions = value, proba = proba, advice = advice)
 
 
 @app.route("/index")
